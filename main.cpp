87960dec23cd8bcfdedd64bdff09cfe735da171c
diverdiver15 <diverdiver15@naver.com>
2024-05-24 09:58:17 +0900

    zsadsadf

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 /*
 1.1~10까지 숫자를 동적을 할당해서 배열에 넣고 
-1. 입력받은 숫자를 임의 위치에 추가해서 배열 크기를 늘리고 
-2. 출력
+2. 입력받은 숫자를 임의 위치에 추가해서 배열 크기를 늘리고 
+3. 출력
 */
 #include <iostream>
 using namespace std;
+//1.1~10까지 숫자를 동적을 할당해서 배열에 넣고
 
 int main()
 {
@@ -16,15 +17,14 @@ int main()
 		Numbers[i] = i + 1;
 	}
 
+    int position = rand() % (Size + 1);//임의위치
+
 	int inputNumber;
-	cout << " 추가할 숫자 입력 : ";
+	cout << " 추가할 숫자 입력 : "; //입력하기 
 	cin >> inputNumber;
+    
     int* Temp = new int[Size + 1]; 
-
-    
-    int position = rand() % (Size + 1);
-
-    
+//배열 크기 늘리기
     for (int i = 0, j = 0; i < Size + 1; ++i) {
         if (i == position) {
             Temp[i] = inputNumber; 
@@ -35,7 +35,7 @@ int main()
         }
     }
 
-    
+    //기존 배열 없애고 새배열
     delete[] Numbers;
     Numbers = Temp;
 
